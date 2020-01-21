a1c25ecf45c2172167aac068d3c0275e839e8a0b
ixxxygrek <57226999+ixxxygrek@users.noreply.github.com>
2020-01-21 22:55:28 +0400

    Add files via upload

---
+++ L405/script.js
@@ -0,0 +1,57 @@
+let students = [
+    {
+        id: 1,
+        name: 'gio',
+        gpa: 3
+    },
+    {
+        id: 2,
+        name: 'oto',
+        gpa: 1.2
+    },
+    {
+        id: 3,
+        name: 'salo',
+        gpa: 2.5
+    },
+    {
+        id: 4,
+        name: 'tamo',
+        gpa: 1.5
+    },
+    {
+        id: 5,
+        name: 'saxlshi damrhca',
+        gpa: 2
+    }
+];
+
+// function myList(array, mingpa = 2) {
+//     let myArray = array.filter(x => x.gpa > mingpa).map((x) => x.gpa);
+//     //    for (let x of array) {
+//     //        if (x.gpa >= mingpa) {
+//     //            myArray.push(x.gpa);
+//     //        }
+//     //
+//     //    }
+
+//     let sum = 0;
+//     for (var i = 0; i < myArray.length; i++) {
+//         sum += myArray[i];
+
+//     }
+//     let avg = sum / (myArray.length > 0 ? myArray.length : 1);
+
+//     return avg;
+// }
+
+function myList(list,mingpa = 2){
+    let sum = 0;
+    let myArray = list.filter((x) => x.gpa > mingpa).map((y) => y.gpa);
+    myArray.map((h) => sum+=h);
+    let avg = sum / myArray.length
+
+    return avg
+}
+
+console.log(myList(students, 2));
